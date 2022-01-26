1d5ca08775e90cfb0bb36bdc57bb52c72213500c
aajisahu <aajisahu@gmail.com>
2022-01-26 21:06:48 +0530

    Added: added validation for input field and printing error message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignment';
-  
-  imgUrl="../assets/Bridgelabzz.jpg";
 
-  url="https://www.bridgelabz.com/";
+  imgUrl = "../assets/Bridgelabzz.jpg";
 
-  userName:string="";
+  url = "https://www.bridgelabz.com/";
 
-  ngOnInit():void{
+  userName: string = "";
+  nameError: string = "";
 
-    this.title="Hello form Bridgelabz";
+  ngOnInit(): void {
+
+    this.title = "Hello form Bridgelabz";
 
   }
 
 
-  onClick($event:any){
-    console.log("Save button is clicked!" ,$event);
+  onClick($event: any) {
+    console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
   }
 
+  onInput($event:any){
+    console.log("Change event occured !", $event.data);
+    const nameRegex= RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    this.nameError="Name is incorrect!"
+  }
 
 }
